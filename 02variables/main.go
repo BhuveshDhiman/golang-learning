8589edfafeb65bf6b6405847234e1d6bcd368b85
Bhuvesh Dhiman <67685811+BhuveshDhiman@users.noreply.github.com>
2021-09-08 19:06:22 +0530

    first commit

---
+++ 02variables/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+const LoginToken string = "qwerty" // capital means public
+
+func main() {
+	var username string = "Bhuvesh"
+	fmt.Println(username)
+	fmt.Printf("Variable is of type: %T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type: %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("Variable is of type: %T \n", smallVal)
+
+	var smallFloat float64 = 255.54543656323442545545
+	fmt.Println(smallFloat)
+	fmt.Printf("Variable is of type: %T \n", smallFloat)
+
+	// Default values and some aliases
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type: %T \n", anotherVariable)
+
+	var anotherString string
+	fmt.Println(anotherString)
+	fmt.Printf("Variable is of type: %T \n", anotherString)
+
+	// implicit type
+	var website = "bhuveshdhiman.me"
+	fmt.Println(website)
+
+	// no var style
+	numberOfUsers := 300000.5
+	fmt.Println(numberOfUsers)
+
+	fmt.Println(LoginToken)
+	fmt.Printf("Variable is of type: %T \n", LoginToken)
+
+}
